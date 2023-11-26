18463221857b6b312d9719c8c8e8529309ad7884
Roman Golovkov <romangolovkov@mail.ru>
2023-11-26 14:58:08 +0800

    Lesson_2 solution_2

---
+++ lesson_2/src/solution_2.py
@@ -0,0 +1,4 @@
+first_number = int(input('Введите первое число'))
+second_number = int(input('Введите второе число'))
+third_number = int(input('Введите третье число'))
+print(f'Среднеее арифмтическое трёх введенных чисел равно {(first_number + second_number + third_number) / 3}')
