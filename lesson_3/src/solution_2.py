2a6649be5480926a3d64452b7e7163ee3181d177
Roman Golovkov <romangolovkov@mail.ru>
2023-11-26 15:28:59 +0800

    Lesson_3 solution_2

---
+++ lesson_3/src/solution_2.py
@@ -0,0 +1,4 @@
+worker_1, worker_2, worker_3, worker_4 = 'Света', 'Олег', 'Маша', 'Паша'
+print(f'В чётные дни работают: {worker_1} и {worker_2}')
+print()
+print(f'В нечётные дни работают: {worker_3} и {worker_4}')
