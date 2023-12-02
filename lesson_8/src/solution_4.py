ca86e386f212f075fd6815065e1b2e19810634ac
Roman Golovkov <romangolovkov@mail.ru>
2023-12-02 17:12:39 +0800

    Lesson_8 solution_4

---
+++ lesson_8/src/solution_4.py
@@ -0,0 +1,14 @@
+workers = ['Снабжение, Менеджер, Иванов', 'Дизайн, Дизайнер, Смирнов', 'Снабжение, Менеджер, Петров', 'Дизайн, Иллюстратор, Сидоров', 'Маркетинг, Аналитик, Сергеев', 'Дизайн, Дизайнер, Васильев']
+workers = [worker.split(', ') for worker in workers]
+
+supply: dict[str, str] = {}
+design: dict[str, str] = {}
+
+for worker in workers:
+    if worker[0] == 'Снабжение':
+        supply[worker[1]] = worker[2]
+    elif worker[0] == 'Дизайн':
+        design[worker[1]] = worker[2]
+
+print(f'Снабжение: {supply}')
+print(f'Снабжение: {design}')
