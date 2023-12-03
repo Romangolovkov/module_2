839db0266b0dd1a2a9cfac382a5c44866fc25faa
Roman Golovkov <romangolovkov@mail.ru>
2023-12-03 23:02:47 +0800

    Lesson_10 corrected solution_3

--- lesson_10/src/solution_3.py
+++ lesson_10/src/solution_3.py
@@ -1,8 +1,8 @@
-def best_worker():
-    quantity = int(input('Введите количество сотрудников: '))
-    workers = [input(f'Введите имя {i + 1}-го сотрудника: ') for i in range(quantity)]
-    tasks = [int(input(f'Введите количество задач, которые выполнил {worker}: ')) for worker in workers]
-    workers_dict = dict(zip(workers, tasks))
+def best_worker() -> list[str]:
+    quantity: int = int(input('Введите количество сотрудников: '))
+    workers: list[str] = [input(f'Введите имя {i + 1}-го сотрудника: ') for i in range(quantity)]
+    tasks: list[int] = [int(input(f'Введите количество задач, которые выполнил {worker}: ')) for worker in workers]
+    workers_dict: dict[str, int] = dict(zip(workers, tasks))
     return [keys for keys, values in workers_dict.items() if values == max(workers_dict.values())]
 
 
