3c98293a79d54970df0550f2ec045f25751426f4
Roman Golovkov <romangolovkov@mail.ru>
2023-12-03 17:51:22 +0800

    Lesson_8 solution_3 corrected

--- lesson_8/src/solution_3.py
+++ lesson_8/src/solution_3.py
@@ -1,7 +1,7 @@
 products: dict[str, int] = {'Яблоко': 100, 'Банан': 80, 'Кофе': 250, 'Чай': 150}
 
 for key in products:
-    if products[key] == max(list(products.values())):
+    if products[key] == max(products.values()):
         print(f'Самый дорогой: {key} - {products[key]} руб.')
-    elif products[key] == min(list(products.values())):
+    elif products[key] == min(products.values()):
         print(f'Самый дешёвый: {key} - {products[key]} руб.')
