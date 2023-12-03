16bdf73d88f29fc963c00d4ea6faa7b0db5437b4
Roman Golovkov <romangolovkov@mail.ru>
2023-12-03 18:21:00 +0800

    Lesson_8 solution_2 corrected

--- lesson_8/src/solution_2.py
+++ lesson_8/src/solution_2.py
@@ -1,7 +1,21 @@
-words: dict[str, str] = {'Красивый': 'Прекрасный', 'Уродливый': 'Некрасивый', 'Сложный': 'Запутанный', 'Простой': 'Легкий', 'Прекрасный': 'Красивый', 'Некрасивый': 'Уродливый', 'Запутанный': 'Сложный', 'Легкий': 'Простой'}
-input_word: str = input('Введите слово: ')
+input_word = 1
+words: dict[str, str] = {'Красивый': 'Прекрасный', 'Уродливый': 'Некрасивый', 'Сложный': 'Запутанный', 'Простой': 'Легкий'}
+print()
 
-if input_word in words:
-    print(f'Синоним: {words[input_word]}')
-else:
-    print('Данного слова в словаре нет')
+while input_word != '0':
+    input_word: str = input('Введите слово для получения синонима, либо "0" для выхода: ')
+
+    if input_word in words:
+        print(f'Синоним: {words[input_word]}')
+
+    elif input_word in words.values():
+        for key, val in words.items():
+            if input_word == val:
+                print(f'Синоним: {key}')
+
+    elif input_word != '0':
+        words[input_word] = input('Данного слова в словаре нет, введите его синоним, чтобы добавить в словарь: ')
+
+print()
+print(f'Словарь синонимов: {words}')
+print()
