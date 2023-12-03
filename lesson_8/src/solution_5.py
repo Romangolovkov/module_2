313bdcebd07d29020d5a207669c70cf4c569f00a
Roman Golovkov <romangolovkov@mail.ru>
2023-12-03 17:51:52 +0800

    Lesson_8 solution_5 corrected

--- lesson_8/src/solution_5.py
+++ lesson_8/src/solution_5.py
@@ -6,9 +6,7 @@
 first_shop: set[str] = set(input('Введите товары первого магазина: ').split(', '))
 second_shop: set[str] = set(input('Введите товары второго магазина: ').split(', '))
 
-common_products: set[str] = first_shop & second_shop
-
 print()
-print(f'Только в первом магазине: {", ".join(list(first_shop - common_products))}')
-print(f'Только во втором магазине: {", ".join(list(second_shop - common_products))}')
+print(f'Только в первом магазине: {", ".join(list(first_shop - second_shop))}')
+print(f'Только во втором магазине: {", ".join(list(second_shop - first_shop))}')
 print()
