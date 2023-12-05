727cd4f0f676c27b3d5ecc03c0cb61851efe9dd4
Roman Golovkov <romangolovkov@mail.ru>
2023-12-05 15:52:33 +0800

    Lesson_9 corrected solution_2

--- lesson_9/src/solution_2.py
+++ lesson_9/src/solution_2.py
@@ -1,5 +1,5 @@
 def convert_to_hex(rgb: tuple[int]) -> str:
-    return f'{rgb[0]:X}{rgb[1]:X}{rgb[2]:X}'
+    return f'{rgb[0]:02X}{rgb[1]:02X}{rgb[2]:02X}'
     
 
 print(f'HEX: {convert_to_hex((0, 0, 255))}')
