ffb2452378f8228f74586a812ac5cf2b9f6ad845
Roman Golovkov <romangolovkov@mail.ru>
2023-12-06 23:18:44 +0800

    Lesson 10 finished solution 3

--- lesson_10/src/solution_3.py
+++ lesson_10/src/solution_3.py
@@ -1,2 +1,9 @@
-a = [1, 1, 1]
-print(a[3])
+def best_worker(workers: dict[str, int]) -> str:
+    return ', '.join([keys for keys, values in workers.items() if values == max(workers.values())])
+
+
+workers: dict[str, int] = {'Анна': 5, 'Боб': 7, 'Сюзан': 9}
+
+print()
+print(f'Самый ответственный работник: {best_worker(workers)}')
+print()
