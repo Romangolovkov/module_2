aaf65f28d07d9ef8fab919b28b573669c8739238
Roman Golovkov <romangolovkov@mail.ru>
2023-12-20 15:23:24 +0800

    Lesson 12 solution 2 corrected typing

--- lesson_12/src/solution_2.py
+++ lesson_12/src/solution_2.py
@@ -1,18 +1,18 @@
 from typing import Callable
 
 
-def check_order(order):
+def check_order(order: dict[str, int|str]) -> bool:
     return bool(order['items'])
 
 
-def package_order(order):
+def package_order(order: dict[str, int|str]) -> str:
     if check_order(order):
         return f'Упакован заказ {order["id"]}'
     else:
         return f'Заказ {order["id"]} пуст'
 
 
-def send_order(check_order, package_order, order):
+def send_order(check_order: Callable[[dict[str, int|str]], bool], package_order: Callable[[dict[str, int|str]], str], order: dict[str, int|str]) -> str:
     return 'Отправка: ' + package_order(order)
 
 
