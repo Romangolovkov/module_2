90f3060298c4370856ad5a5119757dc0dfa8890c
Roman Golovkov <romangolovkov@mail.ru>
2023-12-21 21:08:14 +0800

    Lesson 12 solution 1 corrected typing

--- lesson_12/src/solution_1.py
+++ lesson_12/src/solution_1.py
@@ -2,14 +2,14 @@ from typing import Callable
 
 
 def summarize_data(argument: float):
-    print(f'Среднее значение: {argument}')
+    print(f'Среднее значение: {int(argument)}') if argument % 1 == 0 else print(f'Среднее значение: {argument}')
 
 
-def process_data(numbers: list[int]) -> Callable[[float], None]:
+def process_data(numbers: list[int]):
     return summarize_data(sum(numbers) / len(numbers))
 
 
-def collect_data(list_of_numbers: list[int]) -> Callable[list[int], Callable[[float], None]]:
+def collect_data(list_of_numbers: list[int]):
     return process_data(list_of_numbers)
 
 
