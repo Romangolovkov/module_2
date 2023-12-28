092ace70d579898b0e6a2797a03df6eb6db0d125
Roman Golovkov <romangolovkov@mail.ru>
2023-12-28 17:45:55 +0800

    Lesson 12 solution 2 finished

--- lesson_12/src/solution_2.py
+++ lesson_12/src/solution_2.py
@@ -6,14 +6,13 @@ def check_order(order: dict[str, int|str]) -> bool:
 
 
 def package_order(order: dict[str, int|str]) -> str|None:
-    if check_order(order):
         return f'Упакован заказ {order["id"]}'
 
 
 def send_order(check_order: Callable[[dict[str, int|str]], bool], package_order: Callable[[dict[str, int|str]], str], order: dict[str, int|str]) -> str:
-    if check_order(order) == False:
-        return f'Отправка: Заказ {order["id"]} пуст'
-    return 'Отправка: ' + package_order(order)
+    if check_order(order):
+        return 'Отправка: ' + package_order(order)
+    return f'Отправка: Заказ {order["id"]} пуст'
 
 
 order1 = {'id': 1, 'items': ['book', 'pen']}
