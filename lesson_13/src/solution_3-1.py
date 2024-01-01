433a36cb4092c54fc6eebbf2c5aafdd3792de219
Roman Golovkov <romangolovkov@mail.ru>
2024-01-01 15:23:54 +0800

    Lesson 13 solution 3-1

---
+++ lesson_13/src/solution_3-1.py
@@ -0,0 +1,26 @@
+from typing import Callable
+
+cashe: list[tuple, dict, int] = []
+
+
+def decorator_cashe(func: Callable[[tuple, dict], int]) -> Callable[[tuple, dict], None]:
+    def wrapper(*args, **kwargs):
+        if (args, kwargs) in cashe:
+            print(f'Загрузили из кеша: {cashe[cashe.index((args, kwargs)) + 1]}')
+        else:
+            print(f'Посчитали цену: {func(*args, **kwargs)}')
+            cashe.append((args, kwargs))
+            cashe.append(func(*args, **kwargs))
+            
+    return wrapper
+
+
+@decorator_cashe
+def calculate_project_cost(*args, **kwargs) -> int:
+    return 3000
+
+
+calculate_project_cost('Логотип', 'малый бизнес')
+calculate_project_cost('Логотип', 'малый бизнес')
+calculate_project_cost({1, 2, 3})
+calculate_project_cost({1, 2, 3})
