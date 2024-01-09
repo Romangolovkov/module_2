d151108403c79a705c5c8e09c3d72205b70ccab5
Roman Golovkov <romangolovkov@mail.ru>
2024-01-09 23:56:52 +0800

    Lesson 14 solution 2 corrected

--- lesson_14/src/solution_2.py
+++ lesson_14/src/solution_2.py
@@ -1,6 +1,6 @@
 def func(prices: list[int]) -> list[int]:
     prices: list[int] = [round(price, -2) for price in prices]
-    return list(filter(lambda price: price % 100 == 0, prices))
+    return list(set(filter(lambda price: price % 100 == 0, prices)))
 
 
 print(func([99, 150, 200, 349, 501]))
