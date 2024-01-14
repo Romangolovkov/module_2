6c21684a5a779dcf91ccd11317ca1ba2ca381a01
Roman Golovkov <romangolovkov@mail.ru>
2024-01-14 15:36:37 +0800

    Lesson 16 solution 2

---
+++ lesson_16/src/solution_2.py
@@ -0,0 +1,8 @@
+def new_shelves_func(shelves: str, products: str) -> list[list[int]]:
+    shelves: list[int] = [int(i) for i in shelves.split(',')]
+    products: list[int] = [int(i) for i in products.split(',')]
+    return [[shelves[i], products[i]] for i in range(len(shelves))]
+
+
+print(new_shelves_func('4,5,6', '2,3,5'))
+print(new_shelves_func('3,4', '1,4'))
