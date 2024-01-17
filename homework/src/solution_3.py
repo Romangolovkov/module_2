a068d332a8b064fde826e4513fed51a947d10f6a
Roman Golovkov <romangolovkov@mail.ru>
2024-01-17 16:21:53 +0800

    homework solution 3 finished

--- homework/src/solution_3.py
+++ homework/src/solution_3.py
@@ -1,6 +1,22 @@
 def bracket_balance(brackets: str) -> bool:
     brackets = [bracket for bracket in brackets]
-    while brackets != []:
-        if brackets[0] == '(' 
+    if len(brackets) % 2 != 0:
+        return False
+    else:
+        for i in range(len(brackets) // 2):
+            if (brackets[0] == '(' and brackets[-1] == ')') \
+            or ((brackets[0] == '[' and brackets[-1] == ']')) \
+            or ((brackets[0] == '{' and brackets[-1] == '}')):
+                brackets.pop(0)
+                brackets.pop(-1)
+            else:
+                return False
+                break
+        return True
 
-bracket_balance('sfgdhbh')
+
+print(bracket_balance('[(])'))
+print(bracket_balance('}'))
+print(bracket_balance('[]['))
+print(bracket_balance(')('))
+print(bracket_balance('{([])}'))
