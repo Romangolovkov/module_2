4d5da9671a2c1859f7f86a92937deb826547280a
Roman Golovkov <romangolovkov@mail.ru>
2024-01-17 15:50:03 +0800

    homework solution 3 started

---
+++ homework/src/solution_3.py
@@ -0,0 +1,6 @@
+def bracket_balance(brackets: str) -> bool:
+    brackets = [bracket for bracket in brackets]
+    while brackets != []:
+        if brackets[0] == '(' 
+
+bracket_balance('sfgdhbh')
